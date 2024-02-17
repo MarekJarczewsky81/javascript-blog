f8dfeda0616e5b806e27853de6418fef28895cf8
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-02-17 01:31:27 +0100

    Added functions to script.js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+function titleClickHandler(event) {
+    const clickedElement = this;
+    console.log("Link was clicked!");
+  
+
+  /* remove class 'active' from all article links  */
+
+  const activeLinks = document.querySelectorAll(".titles a.active");
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove("active");
+  }
+
+  /* add class 'active' to the clicked link */
+
+  console.log("clickedElement:", clickedElement);
+
+  /* remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll(".titles.active");
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove("active");
+  }
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll(".titles a");
+
+for (let link of links) {
+  link.addEventListener("click", titleClickHandler);
+}
